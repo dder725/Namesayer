c887b2f7e9ea73224bfafd62d3c220111399a23b
Daria <dder725@aucklanduni.ac.nz>
2018-09-17 16:18:30 +1200

    Created a controller class and added it to the main
    Used FXML file to add the view to the controller

--- NameSayer/src/application/Main.java
+++ NameSayer/src/application/Main.java
@@ -1,20 +1,29 @@
 package application;
-	
+
+import GUI.*;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			FXMLLoader fxmlLoader = new FXMLLoader();
+			Pane p = fxmlLoader.load(getClass().getResource("/GUI/MainWindow.fxml").openStream());
+			MainWindowController mainWindowController = (MainWindowController) fxmlLoader.getController();
+			
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			root.getChildren().add(p);
+			Scene scene = new Scene(root,726,573);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
