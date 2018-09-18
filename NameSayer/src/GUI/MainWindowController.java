09232ba36aad78a635e253de7954e7feef34a366
Daria <dder725@aucklanduni.ac.nz>
2018-09-18 23:49:33 +1200

    Stubs for button methods

--- NameSayer/src/GUI/MainWindowController.java
+++ NameSayer/src/GUI/MainWindowController.java
@@ -2,4 +2,35 @@ package GUI;
 
 public class MainWindowController {
 
+	public void addToPractice() {
+		
+	}
+	
+	public void removeFromPractice() {
+		
+	}
+	
+	public void playRecording() {
+		
+	}
+	
+	public void viewRecordings() {
+		
+	}
+	
+	public void markBad() {
+		
+	}
+	
+	public void addNewName() {
+		
+	}
+	
+	public void practice() {
+		
+	}
+	
+	public void randomiseList() {
+		
+	}
 }
