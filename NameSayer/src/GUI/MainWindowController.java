493a32cf567d24b17dfcadd94daf49a4388fd260
Daria <dder725@aucklanduni.ac.nz>
2018-09-18 23:57:32 +1200

    Bound the recordings dialog to the main window

--- NameSayer/src/GUI/MainWindowController.java
+++ NameSayer/src/GUI/MainWindowController.java
@@ -1,36 +1,52 @@
 package GUI;
 
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class MainWindowController {
 
 	public void addToPractice() {
-		
+
 	}
-	
+
 	public void removeFromPractice() {
-		
+
 	}
-	
+
 	public void playRecording() {
-		
+
 	}
-	
+
 	public void viewRecordings() {
-		
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("ViewRecordingsWindow.fxml"));
+			ViewRecordingsController viewRecordings = (ViewRecordingsController) loader.getController();
+			Parent content = (Parent) loader.load();
+			Stage stage = new Stage();
+			stage.setScene(new Scene(content));
+			stage.show();
+		} catch (IOException e) {
+		}
 	}
-	
+
 	public void markBad() {
-		
+
 	}
-	
+
 	public void addNewName() {
-		
+
 	}
-	
+
 	public void practice() {
-		
+
 	}
-	
+
 	public void randomiseList() {
-		
+
 	}
 }
---
+++ NameSayer/src/GUI/ViewRecordingsController.java
@@ -0,0 +1,8 @@
+package GUI;
+
+public class ViewRecordingsController {
+
+	public String getRecordingName() {
+		return "This is a stub";
+	}
+}
