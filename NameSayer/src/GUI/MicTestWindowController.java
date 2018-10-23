af2ba9b407658fa927b8efc2fad40e9c7637de7d
dder725 <dder725@aucklanduni.ac.nz>
2018-10-23 16:16:14 +1300

    Implemented mic test

--- NameSayer/src/GUI/MicTestWindowController.java
+++ NameSayer/src/GUI/MicTestWindowController.java
@@ -1,16 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package GUI;
 
-import application.Name;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import com.jfoenix.controls.JFXProgressBar;
 
-public class MicTestWindowController {
-	
-	public void listen() {
-		Audio audio = new Audio();
-		audio.playRecording("\"audio.wav\"");
-	}
-	
-	public void setName(Name name) {}
-	
-	public void PWreference(PracticeWindowController pw) {}
-	
+public class MicTestWindowController implements Initializable {
+    
+	@FXML private JFXProgressBar level;
+    @FXML 
+    private void testMic() {
+    	//Bind the results of the recorder to the ProgressBar
+        new Thread(new Recorder(level)).start();
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+ 
+ 
 }
